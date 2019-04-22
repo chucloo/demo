57b0024391d909fbaf896249856dc2c2e2ddefdf
Ufinity <Ufinity@CC-Loo>
2019-04-22 18:23:52 +0800

    Added Test

--- demo/test/demo/CalculatorTest.java
+++ demo/test/demo/CalculatorTest.java
@@ -1,33 +1,33 @@
 package demo;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
-class CalculatorTest {
+public class CalculatorTest {
 
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
 	}
 
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
 	}
 
-	@BeforeEach
-	void setUp() throws Exception {
+	@Before
+	public void setUp() throws Exception {
 	}
 
-	@AfterEach
-	void tearDown() throws Exception {
+	@After
+	public void tearDown() throws Exception {
 	}
 
 	@Test
-	void testAdd() {
+	public void testAdd() {
 		Calculator calculator = new Calculator();
 	    int a = 1234;
 	    int b = 5678;
@@ -50,5 +50,6 @@ class CalculatorTest {
 	 
 	    assertEquals(expected, actual);
 	}
+
 
 }
