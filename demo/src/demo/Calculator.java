8cd3f3e79c33ff2dd8e829f3e34cc454df824723
Ufinity <Ufinity@10.10.1.158>
2019-04-25 14:59:35 +0800

    Added non-compliance code

--- demo/src/demo/Calculator.java
+++ demo/src/demo/Calculator.java
@@ -5,7 +5,19 @@ public class Calculator {
 		// TODO Auto-generated constructor stub
 		
 		public int addition(int num1, int num2) {
-	        return (num1 + num2);
+	        
+			// dummy code
+			int param = 1;
+			
+			if (param == 1)
+				  ; // do nothig
+			else if (param == 2)
+				  ; // do nothing
+			else if (param == 1)  // Noncompliant
+				; // do nothing
+			
+			
+			return (num1 + num2);
 	    }
 
 	    public int substraction(int num1, int num2) {
